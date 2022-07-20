ad2d733e28b01afd4783be02f5ea58d190845c9b
Karol <karolek1443@gmail.com>
2022-07-20 10:57:54 +0200

    modified shit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,27 @@
 'use strict';
 
-import Questionnaire from './classes/questionnaire.class.js';
+// import Questionnaire from './classes/questionnaire.class.js';
 
-const questionnaire = new Questionnaire(
-  'What is your favourite programming language?',
-  ['1: JavaScript', '2: Python', '3: Rust', '4: C++'],
-  [...new Array(4).fill(0)]
-);
+const questionnaire = {
+  question: 'What is your favourite programming language?',
+  options: {
+    1: 'JavaScript',
+    2: 'Python',
+    3: 'Rust',
+    4: 'C++',
+  },
+}
 
-const message = `${questionnaire.question}\n${questionnaire.options.join('\n')}`;
-const poll_btn = document.querySelector('.poll');
+const highest = function () {
+  const x = Object.keys(questionnaire.options).map(Number).fill(0);
+  return x;
+}
 
-poll_btn.addEventListener('click', () => questionnaire.askAQuestion(message));
+const results = highest();
+
+console.log(results);
+
+// const message = `${questionnaire.question}\n${questionnaire.options}`;
+// const poll_btn = document.querySelector('.poll');
+
+// poll_btn.addEventListener('click', () => questionnaire.askAQuestion(message));
